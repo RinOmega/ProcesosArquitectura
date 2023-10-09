0e02a0ba35afe8bcb3a4564084c7c9f6ba195bd0
NOELIA GONZÁLEZ SÁNCHEZ <Noelia.Gonzalez12@alu.uclm.es>
2023-10-09 18:07:44 +0200

    añadida interfaz

--- cliente/controlWeb.js
+++ cliente/controlWeb.js
@@ -1,8 +1,29 @@
-function ControlWeb(){
-    this.mostrarAgregarUsuarios = function() {
-        let cadena = '<div class="form-group">';
-        cadena = cadena + '<label for="nick">Introduce el nick:</label>';
+function ControlWeb()
+{
+    this.mostrarAgregarUsuario = function()
+    {
+        let cadena = 
+        `
+            <div class="form-group" id="mAU">
+                <label for="usr">Introduce el nombre:</label>
+                <input id="nick" type="text" class="form-control" id="usr">
+                <button id="btnAU" type="submit" class="btn btn-primary">Confirmar</button>
+            </div>
+        `;
 
-    };
-    
+        $("#au").append(cadena);
+        
+        $("#btnAU").on("click",function(){ 
+            let nick=$("#nick").val(); 
+            rest.agregarUsuario(nick)
+            $("#mAU").remove();
+            console.log('Hola')
+        })
+    }
+
+    this.mostrarMsg=function(msg){
+        $('#mMsg').remove();
+        let cadena='<h2 id ="mMsg">'+msg+'</h2>';
+        $('#mMsg').append(cadena);
+    }
 }
